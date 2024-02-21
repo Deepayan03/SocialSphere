5906341def240a4c1d4e2c000207ca6eb061a0ef
Deepayan03 <121600646+rider03@users.noreply.github.com>
2024-02-21 14:10:33 +0530

    Deleted extra Client folder

--- client/src/Pages/index.js
+++
@@ -1,6 +0,0 @@
-import Contact from "./Contact/Contact";
-import LoginPage from "./Login/LoginPage";
-import Landing from "./SocialLanding/Landing";
-import SignUp from "./SignUp/SignUp";
-
-export {Contact,LoginPage,Landing,SignUp};
